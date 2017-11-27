8a85a030aaadc385521f53f0f8e037f51a0c7346
Helen Liutongco <bellpickle@gmail.com>
2017-11-27 02:32:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index=0;
 function init() {
   window.addEventListener("keydown", function(event){
-    const key=parseInt(event.key);
-    console.log(key);
+    const key=parseInt(event.which);
+    if(key===code[index]){
+      index++;
+      if(index===code.length){
+        alert("Cheat activated, you cheater")
+      }
+    }
+    else{
+      index=0;
+    }
   })
 }
