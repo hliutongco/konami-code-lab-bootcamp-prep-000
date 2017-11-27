b39104c9866d98d417559cff265c266a8ee0b09a
Helen Liutongco <bellpickle@gmail.com>
2017-11-27 03:01:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index=0;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 function init() {
-  window.addEventListener("keydown", function(event){
-    const key=parseInt(event.which);
-    if(key===code[index]){
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
       index++;
-      if(index===code.length){
-        alert("Hurray!")
-        index=0;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
       }
+    } else {
+      index = 0;
     }
-    else{
-      index=0;
-    }
-  })
+  });
 }
-init();
