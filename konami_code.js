a25a489b95c9e40ebeecfa62cc29513408840200
Helen Liutongco <bellpickle@gmail.com>
2017-11-27 02:34:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
     if(key===code[index]){
       index++;
       if(index===code.length){
-        alert("Cheat activated, you cheater")
+        window.alert("Cheat activated, you cheater")
       }
     }
     else{
