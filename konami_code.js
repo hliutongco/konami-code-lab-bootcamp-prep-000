8d5b060d1c75bcf805d0bda3240018671b9639db
Helen Liutongco <bellpickle@gmail.com>
2017-11-27 03:02:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index=0;
   window.addEventListener("keydown", function(event){
-    const key=parseInt(event.which);
+    const key=parseInt(event.which||event.detail);
     if(key===code[index]){
       index++;
       if(index===code.length){
